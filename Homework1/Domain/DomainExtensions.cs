7dbaa0c69bb5bcd76f4dd1a5631baa479b19c67e
Tan <65961594+aktan132@users.noreply.github.com>
2023-07-21 16:44:37 +0300

    Update DomainExtensions.cs

--- Homework1/Domain/DomainExtensions.cs
+++ Homework1/Domain/DomainExtensions.cs
@@ -1,6 +1,52 @@
-﻿namespace Fuse8_ByteMinds.SummerSchool.Domain;
+using System.Text;
+
+namespace Fuse8_ByteMinds.SummerSchool.Domain;
 
 public static class DomainExtensions
 {
-	// ToDo реализовать экстеншены
+
+    public static bool IsNullOrEmpty<T>(IEnumerable<T> collection)
+    {
+        return collection == null || !collection.GetEnumerator().MoveNext();
+    }
+    public static string JoinToString<T>(this IEnumerable<T> source, string separator)
+    {
+        if (source == null)
+            throw new ArgumentNullException(nameof(source));
+
+        if (separator == null)
+            separator = string.Empty;
+
+        var sb = new StringBuilder();
+        bool firstItem = true;
+
+        foreach (var item in source)
+        {
+            if (!firstItem)
+                sb.Append(separator);
+
+            sb.Append(item);
+            firstItem = false;
+        }
+
+        return sb.ToString();
+
+    }
+        public static int DaysCountBetween(this DateTimeOffset start, DateTimeOffset end)
+        {
+            TimeSpan duration = end - start;
+            return duration.Days;
+        }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
