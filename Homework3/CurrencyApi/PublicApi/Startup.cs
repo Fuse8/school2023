862b5409ee756754c3eb96f626c0dd26edf3f1ab
Sergey Vasnin <sergey.vasnin@fuse8.ru>
2023-07-24 18:18:23 +0600

    добавил дз3

---
+++ Homework3/CurrencyApi/PublicApi/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Fuse8_ByteMinds.SummerSchool.PublicApi;
+
+public class Startup
+{
+	private readonly IConfiguration _configuration;
+
+	public Startup(IConfiguration configuration)
+	{
+		_configuration = configuration;
+	}
+
+	public void ConfigureServices(IServiceCollection services)
+	{
+		services.AddControllers()
+
+			// Добавляем глобальные настройки для преобразования Json
+			.AddJsonOptions(
+				options =>
+				{
+					// Добавляем конвертер для енама
+					// По умолчанию енам преобразуется в цифровое значение
+					// Этим конвертером задаем перевод в строковое занчение
+					options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+				});
+		;
+		services.AddEndpointsApiExplorer();
+		services.AddSwaggerGen();
+	}
+
+	public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+	{
+		if (env.IsDevelopment())
+		{
+			app.UseSwagger();
+			app.UseSwaggerUI();
+		}
+
+		app.UseRouting()
+			.UseEndpoints(endpoints => endpoints.MapControllers());
+	}
+}
