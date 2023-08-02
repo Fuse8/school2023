4be357c18d8e6e3602a93f02ded0c8ec013a5141
Sergey Vasnin <sergey.vasnin@fuse8.ru>
2023-08-02 12:44:30 +0600

    добавил дз 4

---
+++ Homework4/Contracts/ICachedCurrencyAPI.cs
@@ -0,0 +1,35 @@
+﻿namespace Fuse8_ByteMinds.SummerSchool.InternalApi.Contracts;
+
+interface ICachedCurrencyAPI
+{
+	/// <summary>
+	/// Получает текущий курс
+	/// </summary>
+	/// <param name="currencyType">Валюта, для которой необходимо получить курс</param>
+	/// <param name="cancellationToken">Токен отмены</param>
+	/// <returns>Текущий курс</returns>
+	Task<CurrencyDTO> GetCurrentCurrencyAsync(CurrencyType currencyType, CancellationToken cancellationToken);
+
+	/// <summary>
+	/// Получает курс валюты, актуальный на <paramref name="date"/>
+	/// </summary>
+	/// <param name="currencyType">Валюта, для которой необходимо получить курс</param>
+	/// <param name="date">Дата, на которую нужно получить курс валют</param>
+	/// <param name="cancellationToken">Токен отмены</param>
+	/// <returns>Курс на дату</returns>
+	Task<CurrencyDTO> GetCurrencyOnDateAsync(CurrencyType currencyType, DateOnly date, CancellationToken cancellationToken);
+}
+
+// Данные модели использовать не обязательно, можно реализовать свои
+
+/// <summary>
+/// Курс валюты
+/// </summary>
+/// <param name="CurrencyType">Валюта</param>
+/// <param name="Value">Значение курса</param>
+record CurrencyDTO(CurrencyType CurrencyType, decimal Value);
+
+enum CurrencyType
+{
+	Usd, Rub, Kzt,
+}
---
+++ Homework4/Contracts/ICurrencyAPI.cs
@@ -0,0 +1,37 @@
+﻿namespace Fuse8_ByteMinds.SummerSchool.InternalApi.Contracts;
+
+interface ICurrencyAPI
+{
+	/// <summary>
+	/// Получает текущий курс для всех валют
+	/// </summary>
+	/// <param name="baseCurrency">Базовая валюта, относительно которой необходимо получить курс</param>
+	/// <param name="cancellationToken">Токен отмены</param>
+	/// <returns>Список курсов валют</returns>
+	Task<Currency[]> GetAllCurrentCurrenciesAsync(string baseCurrency, CancellationToken cancellationToken);
+
+	/// <summary>
+	/// Получает курс для всех валют, актуальный на <paramref name="date"/>
+	/// </summary>
+	/// <param name="baseCurrency">Базовая валюта, относительно которой необходимо получить курс</param>
+	/// <param name="date">Дата, на которую нужно получить курс валют</param>
+	/// <param name="cancellationToken">Токен отмены</param>
+	/// <returns>Список курсов валют на дату</returns>
+	Task<CurrenciesOnDate> GetAllCurrenciesOnDateAsync(string baseCurrency, DateOnly date, CancellationToken cancellationToken);
+}
+
+// Данные модели использовать не обязательно, можно реализовать свои
+
+/// <summary>
+/// Курс валюты
+/// </summary>
+/// <param name="Code">Код валюты</param>
+/// <param name="Value">Значение курса валют, относительно базовой валюты</param>
+record Currency(string Code, decimal Value);
+
+/// <summary>
+/// Курсы валют на конкретную дату
+/// </summary>
+/// <param name="LastUpdatedAt">Дата обновления данных</param>
+/// <param name="Currencies">Список курсов валют</param>
+record CurrenciesOnDate(DateTime LastUpdatedAt, Currency[] Currencies);
